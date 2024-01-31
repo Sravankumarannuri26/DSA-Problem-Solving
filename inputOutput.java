3be05b516a5d09dce6eacbcdd0e4ea55316e075c
Sravan Kumar Annuri <150375871+Sravankumarannuri26@users.noreply.github.com>
2024-01-31 12:23:55 +0530

    Add files via upload

---
+++ inputOutput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class inputOutput{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name:");
+        int name=sc.nextInt(); 
+        System.out.println(name);
+    }
+}
